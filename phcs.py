7e6ea166d080a13b2bf207cdfbd51db9ca4606ed
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-05-24 17:10:05 -0400

    Added Soft GeoIP Settings

--- phcs.py
+++ phcs.py
@@ -18,19 +18,19 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys,os
+import sys, os
 import threading
-import time,datetime
+import time, datetime
 import argparse
 import Queue
 import ConfigParser
 
-exitError=False
+exitError = False
 try:
     import json
 except ImportError:
     print "json not found\nsudo pip install json"
-    exitError=True
+    exitError = True
 try:
     import gflags
     import httplib2
@@ -42,17 +42,17 @@ try:
 except ImportError:
     print "Google API Client not found"
     print "sudo pip install google-api-python-client"
-    exitError=True
+    exitError = True
 try:
     import pyttsx
 except ImportError:
     print "pyttsx not found\nsudo pip install pyttsx"
-    exitError=True
+    exitError = True
 try:
     import requests
 except ImportError:
     print "requests not found\nsudo pip install requests"
-    exitError=True
+    exitError = True
 try:
     import dateutil.parser
 except ImportError:
@@ -61,67 +61,48 @@ if exitError:
     exit(1)
 
 class phi():
-    def say(self,string):
+    def say(self, string):
         self.say_queue.put(string)
 
     def __read_config(self):
         config = ConfigParser.ConfigParser()
-        config.readfp(open(os.path.abspath(
-                            os.path.join(sys.path[0],'settings.py'))))
+        config.readfp(open(os.path.abspath(os.path.join(sys.path[0], 'settings.conf'))))
         return config
 
-    def __process_command_line_arguments(self,config):
+    def __process_command_line_arguments(self, config):
         parser = argparse.ArgumentParser(
-                            description='Persional Home Computrized Secritary')
-        parser.add_argument("-d","--debug",
-                            action="store_true",
-                            dest="debug",
-                            default=config.get("MAIN","DEBUG"),
-                            help="Enable Debugging")
-        parser.add_argument("-s","--no-speak",
-                            action="store_false",
-                            dest="nospeak",
-                            default=config.get("MAIN","SPEAK"),
-                            help="Disable TTS")
-        parser.add_argument("-z""--zipcode",
-                            action="store",
-                            dest="zipcode",
-                            default=config.get("MAIN","ZIPCODE"),
-                            help="Change Zipcode")
-        parser.add_argument("-u","--units",
-                            action="store",
-                            dest="units",
-                            default=config.get("MAIN","UNITS"),
-                            help="Change Units Imperial/Metric")
+                        description = 'Persional Home Computrized Secritary')
+        parser.add_argument("-d", "--debug", action = "store_true", dest = "debug", default = config.get("MAIN", "DEBUG"), help = "Enable Debugging")
+        parser.add_argument("-s", "--no-speak", action = "store_false", dest = "nospeak", default = config.get("MAIN", "SPEAK"), help = "Disable TTS")
+        parser.add_argument("-z""--postalcode", action = "store", dest = "postal_code", default = config.get("MAIN", "POSTALCODE"), help = "Postal Code used for location based services like weather")
+        parser.add_argument("--soft-postalcode", action = "store_true", dest = "soft_postal_code", default = config.get("MAIN", "USE_SOFT_POSTALCODE"), help = "Try to determine the postal code programmatically.\nNote: you will need to have GEOIP  city data somewhere, and have to set --geo-data-file also")
+        parser.add_argument("--geo-data-file", action = "store", dest = "geo_data_file", default = config.get("MAIN", "GEO_IP_DATA_FILE"), help = "GeoIP Data File Location")
 
-        parser.add_argument("--wunderground",
-                            action="store",
-                            dest="wunderground",
-                            default=config.get("API","WUNDERGROUND_KEY"),
-                            help="Weather Underground API Key")
-        parser.add_argument("--googleid",
-                            action="store",
-                            dest="googid",
-                            default=config.get("API","GOOGLE_ID"),
-                            help="Google ID Key")
-        parser.add_argument("--googlesecret",
-                            action="store",
-                            dest="googsecret",
-                            default=config.get("API","GOOGLE_SECRET"),
-                            help="Google Secret Key")
-        parser.add_argument("--googledevkey",
-                            action="store",
-                            dest="googdevkey",
-                            default=config.get("API","GOOGLE_DEVEL_KEY"),
-                            help="Google Developer Key")
+        parser.add_argument("-u", "--units", action = "store", dest = "units", default = config.get("MAIN", "UNITS"), help = "Change Units Imperial/Metric")
 
-        parser.parse_args(namespace=self)
+        parser.add_argument("--wunderground", action = "store", dest = "wunderground", default = config.get("API", "WUNDERGROUND_KEY"), help = "Weather Underground API Key")
+        parser.add_argument("--googleid", action = "store", dest = "googid", default = config.get("API", "GOOGLE_ID"), help = "Google ID Key")
+        parser.add_argument("--googlesecret", action = "store", dest = "googsecret", default = config.get("API", "GOOGLE_SECRET"), help = "Google Secret Key")
+        parser.add_argument("--googledevkey", action = "store", dest = "googdevkey", default = config.get("API", "GOOGLE_DEVEL_KEY"), help = "Google Developer Key")
+        parser.parse_args(namespace = self)
+
+        if self.soft_postal_code:
+            try:
+                import pygeoip
+                gi = pygeoip.GeoIP(os.path.abspath(self.geo_data_file))
+                ip = requests.get('http://automation.whatismyip.com/n09230945.asp').content
+                record = gi.record_by_addr(ip)
+                if 'postal_code' in record:
+                    self.postal_code = record['postal_code']
+            except ImportError:
+                print "pygeoip not found defaulting to set postalcode " + self.postal_code + "\nTo use soft postal_code:\n\tpip install pygeoip"
+
         unit = self.units.lower()
 
-        if unit in ('i','imperial'):
-            self.units='i'
-        elif unit in ('m','metric'):
-            self.units="m"
+        if unit in ('i', 'imperial'):
+            self.units = 'i'
+        elif unit in ('m', 'metric'):
+            self.units = "m"
         else:
             print "%s not a recognized value for units\nexiting" % unit
             exit(1)
@@ -142,21 +123,21 @@ class phi():
         self.say_queue = Queue.Queue()
         self.lock = threading.Lock()
         #speak thread
-        self.speak_thread = voice_thread(self)
+        self.speak_thread = voiceThread(self)
         self.speak_thread.start()
         #all other threads
-        self.taw_thread = time_and_weather_thread(self)
+        self.taw_thread = timeAndWeatherThread(self)
         self.taw_thread.start()
-        self.cal_thread = calendar_thread(self)
+        self.cal_thread = calendarThread(self)
         self.cal_thread.start()
 
-class voice_thread(threading.Thread):
+class voiceThread(threading.Thread):
 
-    def __init__(self,phi):
+    def __init__(self, phi):
         threading.Thread.__init__(self)
         self.voice_engine = pyttsx.init()
         self.voice_engine.setProperty('rate',
-                        self.voice_engine.getProperty('rate')-50)
+                        self.voice_engine.getProperty('rate') - 50)
         self._stopevent = threading.Event()
         self.phi = phi
 
@@ -174,18 +155,17 @@ class voice_thread(threading.Thread):
             if self.phi.nospeak:
                 self.voice_engine.runAndWait()
 
-    def join(self,timeout=None):
+    def join(self, timeout = None):
         self._stopevent.set()
         self.voice_engine.stop()
-        threading.Thread.join(self,timeout)
+        threading.Thread.join(self, timeout)
 
-class time_and_weather_thread(threading.Thread):
-    def __init__(self,phi):
+class timeAndWeatherThread(threading.Thread):
+    def __init__(self, phi):
         threading.Thread.__init__(self)
         self._stopevent = threading.Event()
         self.phi = phi
-        self.base_url = "http://api.wunderground.com/api/%s/"
-                            % (self.phi.wunderground)
+        self.base_url = "http://api.wunderground.com/api/%s/" % (self.phi.wunderground)
 
     def run(self):
         if not self._stopevent.isSet():
@@ -204,47 +184,33 @@ class time_and_weather_thread(threading.Thread):
             greeting = "Afternoon"
         if now.hour >= 17:
             greeting = "Evening"
-        return "Good %s"
-                    %(greeting) +
-                    now.strftime(
-                        " it is %A %B %dth.  The Time is now %I %M %p")
+        return "Good %s" % (greeting) + now.strftime(" it is %A %B %dth.  The Time is now %I %M %p")
 
     def get_weather(self):
-        url = self.base_url+"/conditions/forecast/astronomy/tide/q/"
-                                    + self.phi.zipcode+".json"
+        url = self.base_url + "/conditions/forecast/astronomy/tide/q/" + self.phi.postal_code + ".json"
         weather = json.loads(requests.get(url).content)
         if self.phi.units == "i":
             current_temp = weather['current_observation']['temp_f']
-            forcast_high = weather['forecast']['simpleforecast']
-                            ['forecastday'][0]['high']['fahrenheit']
-            forcast_low = weather['forecast']
-                            ['simpleforecast']
-                            ['forecastday'][0]['low']['fahrenheit']
+            forcast_high = weather['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit']
+            forcast_low = weather['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit']
         if self.phi.units == "m":
             current_temp = weather['current_observation']['temp_c']
-            forcast_high = weather['forecast']['simpleforecast']
-                            ['forecastday'][0]['high']['celsius']
-            forcast_low = weather['forecast']['simpleforecast']
-                            ['forecastday'][0]['low']['celsius']
+            forcast_high = weather['forecast']['simpleforecast']['forecastday'][0]['high']['celsius']
+            forcast_low = weather['forecast']['simpleforecast']['forecastday'][0]['low']['celsius']
         current_condition = weather['current_observation']['weather']
         current_humidity = weather['current_observation']['relative_humidity']
-        current_string = "It is currently %d degrees and %s "
-                            +"with a humidity of %s."
-                            %(current_temp,current_condition,current_humidity)
-        forcast_condition = weather['forecast']['simpleforecast']
-                            ['forecastday'][0]['conditions']
-        forcast_string = "Today will have a high of %s degrees"+
-                            "low of %s degrees, condistions are: %s."
-                            % (forcast_high,forcast_low,forcast_condition)
-        return (current_string+" "+forcast_string)
+        current_string = "It is currently %d degrees and %s with a humidity of %s." % (current_temp, current_condition, current_humidity)
+        forcast_condition = weather['forecast']['simpleforecast']['forecastday'][0]['conditions']
+        forcast_string = "Today will have a high of %s degrees low of %s degrees, condistions are: %s." % (forcast_high, forcast_low, forcast_condition)
+        return (current_string + " " + forcast_string)
 
-    def join(self,timeout=None):
+    def join(self, timeout = None):
         self._stopevent.set()
-        threading.Thread.join(self,timeout)
+        threading.Thread.join(self, timeout)
 
-class calendar_thread(threading.Thread):
+class calendarThread(threading.Thread):
 
-    def __init__(self,phi):
+    def __init__(self, phi):
         threading.Thread.__init__(self)
         self._stopevent = threading.Event()
         self.phi = phi
@@ -260,36 +226,32 @@ class calendar_thread(threading.Thread):
 
     def login(self):
         FLOW = OAuth2WebServerFlow(
-            client_id=self.phi.googid,
-            client_secret=self.phi.googsecret,
-            scope='https://www.googleapis.com/auth/calendar',
-            user_agent='phi.'+self.phi.__VERSION__)
+            client_id = self.phi.googid,
+            client_secret = self.phi.googsecret,
+            scope = 'https://www.googleapis.com/auth/calendar',
+            user_agent = 'phi.' + self.phi.__version__)
         storage = Storage('calendar.dat')
         credentials = storage.get()
         if credentials is None or credentials.invalid == True:
-          credentials = run(FLOW, storage)
+            credentials = run(FLOW, storage)
         http = httplib2.Http()
         http = credentials.authorize(http)
-        self.service = build(serviceName='calendar', version='v3', http=http,
-               developerKey=self.phi.googdevkey)
+        self.service = build(serviceName = 'calendar', version = 'v3', http = http,
+               developerKey = self.phi.googdevkey)
 
     def get_today_events(self):
-        full_events=[]
-        hour_events=[]
+        full_events = []
+        hour_events = []
         returnString = ""
         now = datetime.datetime.now()
-        start_date = datetime.datetime(now.year,now.month,now.day,
-                                       0,0,0,0,tzinfo=dateutil.tz.tzlocal())
-                                            .isoformat()
-        end_date = datetime.datetime(now.year,now.month,now.day
-                                    ,23,59,59,999999,tzinfo=dateutil.tz.tzlocal())
-                                            .isoformat()
-        calendars =self.service.calendarList().list().execute()
+        start_date = datetime.datetime(now.year, now.month, now.day, 0, 0, 0, 0, tzinfo = dateutil.tz.tzlocal()).isoformat()
+        end_date = datetime.datetime(now.year, now.month, now.day, 23, 59, 59, 999999, tzinfo = dateutil.tz.tzlocal()).isoformat()
+        calendars = self.service.calendarList().list().execute()
         for calendar in calendars['items']:
-            id = calendar['id']
-            events = self.service.events().list(calendarId=id,
-                                                timeMin=start_date,
-                                                timeMax=end_date).execute()
+            cal_id = calendar['id']
+            events = self.service.events().list(calendarId = cal_id,
+                                                timeMin = start_date,
+                                                timeMax = end_date).execute()
             if 'items' in events:
                 for event in events['items']:
                     event_name = event['summary'].lstrip()
@@ -297,58 +259,50 @@ class calendar_thread(threading.Thread):
                     if 'date' in event_start:
                         full_events.append(event_name)
                     else:
-                        time = dateutil.parser.parse(event_start['dateTime'])
-                                    .astimezone(dateutil.tz.tzlocal())
+                        time = dateutil.parser.parse(event_start['dateTime']).astimezone(dateutil.tz.tzlocal())
                         if time.strftime("%p") == "AM":
                             ampm = "A.M."
                         else:
                             ampm = "P.M."
                         if time.minute == 0:
-                            timestr = "%d o'clock %s"
-                                        % (int(time.strftime("%I")),ampm)
+                            timestr = "%d o'clock %s" % (int(time.strftime("%I")), ampm)
                         else:
-                            timestr = "%d %d %s"
-                                        % (int(time.strftime("%I")),
-                                           time.minute,ampm)
+                            timestr = "%d %d %s" % (int(time.strftime("%I")),
+                                           time.minute, ampm)
                         event = {'name':event_name,
                                  'time_str':timestr,
                                  'time':time}
                         hour_events.append(event)
 
-        hour_events = sorted(hour_events,key=lambda event: event['time'])
-        numberEvents = len(full_events)+len(hour_events)
+        hour_events = sorted(hour_events, key = lambda event: event['time'])
+        numberEvents = len(full_events) + len(hour_events)
         if numberEvents == 0:
             return "You have nothing planned for today"
         else:
             if len(hour_events) > 0:
                 if len(hour_events) == 1:
-                    returnString = "You have %d appointment today.\n"
-                                        % len(hour_events)
+                    returnString = "You have %d appointment today.\n" % len(hour_events)
                 else:
-                    returnString = "You have %d appointments today.\n"
-                                        % len(hour_events)
+                    returnString = "You have %d appointments today.\n" % len(hour_events)
                 for event in hour_events:
-                    returnString+="%s at %s.\n"
-                                    % (event['name'],event['time_str'])
+                    returnString += "%s at %s.\n" % (event['name'], event['time_str'])
             if len(full_events) > 0:
-                if len(full_events)==1:
-                    returnString += "You have %d note.\n"
-                                        % len(full_events)
+                if len(full_events) == 1:
+                    returnString += "You have %d note.\n" % len(full_events)
                 else:
-                    returnString += "You have %d notes.\n"
-                                        % len(full_events)
+                    returnString += "You have %d notes.\n" % len(full_events)
                 for event in full_events:
-                    returnString+="%s\n"%event
+                    returnString += "%s\n" % event
             return returnString
         return "You have nothing planned for today"
 
-    def join(self,timeout=None):
+    def join(self, timeout = None):
         self._stopevent.set()
-        threading.Thread.join(self,timeout)
+        threading.Thread.join(self, timeout)
 
-class skeloton_thread(threading.Thread):
+class skelotonThread(threading.Thread):
 
-    def __init__(self,phi):
+    def __init__(self, phi):
         threading.Thread.__init__(self)
         self._stopevent = threading.Event()
         self.phi = phi
@@ -357,12 +311,12 @@ class skeloton_thread(threading.Thread):
         while not self._stopevent.isSet():
             None
 
-    def join(self,timeout=None):
+    def join(self, timeout = None):
         self._stopevent.set()
-        threading.Thread.join(self,timeout)
+        threading.Thread.join(self, timeout)
 
 if __name__ == "__main__":
-    p=phi()
+    p = phi()
     stopEvent = threading.Event()
     while not stopEvent.isSet():
         try:
